54d2fe2a2c7fd077fc27ea09fb705fa134492974
Daniel Neis Araujo <danielneis@gmail.com>
2015-09-23 15:55:52 -0300

    adding boleto skeleton from vendor lib

---
+++ boleto.php
@@ -0,0 +1,87 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This files show courses based on the city 
+ *
+ * @copyright 2015 Daniel Neis
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package local_agenda
+ */
+
+// The following variables are the same defined at vendor/bielsystems/boletophp/boleto_cef.php
+// TODO: implement settings with options to populate boletos
+$dias_de_prazo_para_pagamento = 5;
+$taxa_boleto = 2.95;
+$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias  OU  informe data: "13/04/2006"  OU  informe "" se Contra Apresentacao;
+$valor_cobrado = "2950,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+
+$dadosboleto["inicio_nosso_numero"] = "80";  // Carteira SR: 80, 81 ou 82  -  Carteira CR: 90 (Confirmar com gerente qual usar)
+$dadosboleto["nosso_numero"] = "19525086";  // Nosso numero sem o DV - REGRA: Máximo de 8 caracteres!
+$dadosboleto["numero_documento"] = "27.030195.10";	// Num do pedido ou do documento
+$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+// DADOS DO SEU CLIENTE
+$dadosboleto["sacado"] = "Nome do seu Cliente";
+$dadosboleto["endereco1"] = "Endereço do seu Cliente";
+$dadosboleto["endereco2"] = "Cidade - Estado -  CEP: 00000-000";
+
+// INFORMACOES PARA O CLIENTE
+$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Nonononono";
+$dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
+$dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
+
+// INSTRUÇÕES PARA O CAIXA
+$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
+$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
+
+// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+$dadosboleto["quantidade"] = "";
+$dadosboleto["valor_unitario"] = "";
+$dadosboleto["aceite"] = "";		
+$dadosboleto["especie"] = "R$";
+$dadosboleto["especie_doc"] = "";
+
+// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+// DADOS DA SUA CONTA - CEF
+$dadosboleto["agencia"] = "1565"; // Num da agencia, sem digito
+$dadosboleto["conta"] = "13877"; 	// Num da conta, sem digito
+$dadosboleto["conta_dv"] = "4"; 	// Digito do Num da conta
+
+// DADOS PERSONALIZADOS - CEF
+$dadosboleto["conta_cedente"] = "87000000414"; // ContaCedente do Cliente, sem digito (Somente Números)
+$dadosboleto["conta_cedente_dv"] = "3"; // Digito da ContaCedente do Cliente
+$dadosboleto["carteira"] = "SR";  // Código da Carteira: pode ser SR (Sem Registro) ou CR (Com Registro) - (Confirmar com gerente qual usar)
+
+// SEUS DADOS
+$dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
+$dadosboleto["cpf_cnpj"] = "";
+$dadosboleto["endereco"] = "Coloque o endereço da sua empresa aqui";
+$dadosboleto["cidade_uf"] = "Cidade / Estado";
+$dadosboleto["cedente"] = "Coloque a Razão Social da sua empresa aqui";
+
+// NÃO ALTERAR!
+include("./vendor/bielsystems/boletophp/include/funcoes_cef.php"); 
+include("./vendor/bielsystems/boletophp/include/layout_cef.php");
