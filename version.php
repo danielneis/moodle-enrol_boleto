aa5fdfd1f08955c314516e829e21a7b0ae4819f1
Daniel Neis <danielneis@gmail.com>
2015-11-25 09:32:55 -0200

    adding plugin maturity and release

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015051100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015050500;        // Requires this Moodle version
 $plugin->component = 'enrol_boleto';      // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_RC;
+$plugin->release = 1;
